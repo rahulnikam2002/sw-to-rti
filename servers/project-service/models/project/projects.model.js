638a8ef158e0b31a99b1bdec67915407c17130d8
Rahul Nikam <codewithrahulnikam@gmail.com>
2024-04-05 15:31:54 +0530

    db model created

--- servers/project-service/models/project/projects.model.js
+++ servers/project-service/models/project/projects.model.js
@@ -1,30 +1,48 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-  {
-    userName: {
-      type: String,
-      required: true
+const ProjectSchema = new mongoose.Schema(
+    {
+        projectName: {
+            type: String,
+            required: true
+        },
+        aboutProject: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        projectStartedOn: {
+            type: String,
+            required: true
+        },
+        projectWillEndOn: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        totalInvestment: {
+            type: String
+        },
+        investedTillDate: {
+            type: String
+        },
+        tenderOwner: {
+            type: String
+        },
+        governmentBody: {
+            type: String
+        },
+        projectAddress: {
+            type: String
+        },
+        isStillInWorking: {
+            type: Boolean
+        },
+        reason: {
+            type: String
+        }
     },
-    userEmail: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    userPassword: {
-      type: String,
-      required: true
-    },
-    userNumber: {
-      type: Number,
-      required: true,
-      unique: true
-    },
-    userAvatar: {
-      type: String
-    }
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("UserModel", UserSchema);
